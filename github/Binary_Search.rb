12769265281ce99adbfee58f6b5b2e654986c3f5
Muhasin Rashid <muhasin23@gmail.com>
2018-03-28 14:48:12 +0530

    Binary_Search updated

--- github/Binary_Search.rb
+++ github/Binary_Search.rb
@@ -1,46 +1,53 @@
-# Binary_Search
-# Give the number set as arguments in ascending order
-#count = 0
-#* numbers = ARGV
-#e: t_numbers = Array.new(2,$stdin.gets.chomp.to_i)
-#$stdin.gets.chomp.to_i
-#for number in num>ers
-#  count = count + 1
-#end
-#puts count
-#puts set_numbers
-def binary_search_normal()
-  number = 0
-  print "please enter the size of number set: "
-  size = gets.chomp.to_i
-  ary = Array.new
-  print "please enter #{size} elements in ascending order:\n"
-  while size > number
-    ary.push(gets.chomp.to_i)
-    size -= 1
-  end
-  #bsearch_find = ary.bsearch{|x| x >= 4}
-  #puts bsearch_find
-  last = ary.length - 1
-  first = 0
-  position = 0
-  flag = false
-  print "please enter the number to search:"
-  search_number = gets.chomp.to_i
-  while (first <= last) do
-    position = first + (last - first) / 2
-    if (search_number == ary[position])
-      flag = true
-      puts "number found in position #{position + 1}"
+module BinarySearch
+  class Binarysearch
+    attr_accessor :ary, :search_number
+    @@all = []
+    def get_number_set()
+      number = 0
+      ary = Array.new
+      print "please enter the size of number set: "
+      size = gets.chomp.to_i
+      print "please enter #{size} elements in ascending order:\n"
+      while size > number
+        ary.push(gets.chomp.to_i)
+        size -= 1
+      end
+      @ary = ary
+      @@all.push(self)
     end
-    if search_number > ary[position]
-      first = position + 1
-    else
-      last = position - 1
+    def get_search_number()
+      print "please enter the number to search:"
+      search_number = gets.chomp.to_i
+      @search_number = search_number
+      @@all.push(self)
     end
-  end
-  if flag == false
-      puts "Number not found"
+    def search_number_set()
+      last = self.ary.length - 1
+      first = 0
+      position = 0
+      flag = false
+      while (first <= last) do
+        position = first + (last - first) / 2
+        if (self.search_number == self.ary[position])
+          flag = true
+          puts "number found in position #{position + 1}"
+          return(self.search_number)
+        end
+        if self.search_number > self.ary[position]
+          first = position + 1
+        else
+          last = position - 1
+        end
+      end
+      if flag == false
+          puts "Number not found"
+      end
+    end
   end
 end
-binary_search_normal()
+
+include BinarySearch
+search = Binarysearch.new
+search.get_number_set
+search.get_search_number
+search.search_number_set
